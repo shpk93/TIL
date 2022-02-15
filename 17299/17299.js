b380762dcfa1c2812c23d408705219010ee6bccd
shpk93 <shpk930924@gmail.com>
2022-02-16 03:58:25 +0900

    17299

--- 17299/17299.js
+++ 17299/17299.js
@@ -4,23 +4,29 @@ const splitStr = process.platform === "linux" ? "\n" : "\r\n";
 var input = fs.readFileSync(filepath).toString().split(splitStr);
 
 let N = +input[0];
-
 let arr = input[1].split(" ").map((el) => +el);
 
 function solution(N, arr) {
+  let countBox = {};
+  let answerArr = new Array(N).fill(-1);
   let stack = [];
-  let answer = new Array(N).fill(-1);
+
+  for (let i = 0; i < N; i++) {
+    countBox[arr[i]] ? countBox[arr[i]]++ : (countBox[arr[i]] = 1);
+  }
 
   for (let i = 0; i < N; i++) {
     while (stack.length) {
-      let [value, index] = stack[stack.length - 1];
-      if (value < arr[i]) {
-        answer[index] = arr[i];
+      let now = countBox[arr[i]];
+      let top = stack[stack.length - 1];
+      if (now > countBox[top[0]]) {
+        answerArr[top[1]] = arr[i];
         stack.pop();
       } else break;
     }
     stack.push([arr[i], i]);
   }
-  return answer.join(" ");
+
+  console.log(answerArr.join(" "));
 }
-console.log(solution(N, arr));
+solution(N, arr);
